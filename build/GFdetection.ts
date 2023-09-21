a63d2ac86434b06e3b287b07ad52b51421131cff
akimixu <1527829777@qq.com>
2023-09-21 20:08:10 +0800

    🌟feat(修改git push操作，使用--all推送所有数据源): 修改git push操作，使用--all推送所有数据源

--- build/GFdetection.ts
+++ build/GFdetection.ts
@@ -28,8 +28,40 @@ exec(gitPullCommand, (error, stdout, stderr) => {
   }
 
   // 如果没有冲突，执行 Git push 命令
-  const gitPushCommand = "git push origin master";
-  const gitPushMirrorCommand = "git push mirror master";
+  //   const gitPushCommand = "git push origin master";
+  //   const gitPushMirrorCommand = "git push mirror master";
+  //   exec(gitPushCommand, (error, stdout, stderr) => {
+  //     if (error) {
+  //       log(
+  //         chalk.hex("#646cff")(`Error executing 'git push':`) +
+  //           chalk.red(error.message)
+  //       );
+  //       return;
+  //     }
+
+  //     if (stderr) {
+  //       log(chalk.hex("#646cff")(`Git push stderr:`) + chalk.red(stderr));
+  //       return;
+  //     }
+  //     log(chalk.hex("#646cff")(`Git push origin successful.`));
+  //   });
+
+  //   exec(gitPushMirrorCommand, (error, stdout, stderr) => {
+  //     if (error) {
+  //       log(
+  //         chalk.hex("#646cff")(`Error executing 'git push mirror':`) +
+  //           chalk.red(error.message)
+  //       );
+  //       return;
+  //     }
+  //     if (stderr) {
+  //       log(chalk.hex("#646cff")(`Git push stderr:`) + chalk.red(stderr));
+  //       return;
+  //     }
+  //     log(chalk.hex("#646cff")(`Git push mirror successful.`));
+  //   });
+
+  const gitPushCommand = "git push --all"; // 同时推送所有数据源
   exec(gitPushCommand, (error, stdout, stderr) => {
     if (error) {
       log(
@@ -40,24 +72,10 @@ exec(gitPullCommand, (error, stdout, stderr) => {
     }
 
     if (stderr) {
-      log(chalk.hex("#646cff")(`Git push stderr:`) + chalk.red(stderr));
+      log(chalk.hex("#646cff")(`Git push --all [stderr]:`) + chalk.red(stderr));
       return;
     }
-    log(chalk.hex("#646cff")(`Git push origin successful.`));
-  });
-
-  exec(gitPushMirrorCommand, (error, stdout, stderr) => {
-    if (error) {
-      log(
-        chalk.hex("#646cff")(`Error executing 'git push mirror':`) +
-          chalk.red(error.message)
-      );
-      return;
-    }
-    if (stderr) {
-      log(chalk.hex("#646cff")(`Git push stderr:`) + chalk.red(stderr));
-      return;
-    }
-    log(chalk.hex("#646cff")(`Git push mirror successful.`));
+    // 推送成功
+    log(chalk.hex("#646cff")("Git push --all:") + chalk.green(` successful.`));
   });
 });

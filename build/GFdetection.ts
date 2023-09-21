29b5492cb3585b58a64935d9f49945b9f910dda2
akimixu <1527829777@qq.com>
2023-09-21 20:25:58 +0800

    🌠refactor(修改git push --all指令为两个指令和): 修改git push --all指令为两个指令和

--- build/GFdetection.ts
+++ build/GFdetection.ts
@@ -28,8 +28,40 @@ exec(gitPullCommand, (error, stdout, stderr) => {
   }
 
   // 如果没有冲突，执行 Git push 命令
-  const gitPushCommand = "git push origin master";
-  const gitPushMirrorCommand = "git push mirror master";
+  //   const gitPushCommand = "git push origin master";
+  //   const gitPushMirrorCommand = "git push mirror master";
+  //   exec(gitPushCommand, (error, stdout, stderr) => {
+  //     if (error) {
+  //       log(
+  //         chalk.hex("#646cff")(`Error executing 'git push':`) +
+  //           chalk.red(error.message)
+  //       );
+  //       return;
+  //     }
+
+  //     if (stderr) {
+  //       log(chalk.hex("#646cff")(`Git push stderr:`) + chalk.red(stderr));
+  //       return;
+  //     }
+  //     log(chalk.hex("#646cff")(`Git push origin successful.`));
+  //   });
+
+  //   exec(gitPushMirrorCommand, (error, stdout, stderr) => {
+  //     if (error) {
+  //       log(
+  //         chalk.hex("#646cff")(`Error executing 'git push mirror':`) +
+  //           chalk.red(error.message)
+  //       );
+  //       return;
+  //     }
+  //     if (stderr) {
+  //       log(chalk.hex("#646cff")(`Git push stderr:`) + chalk.red(stderr));
+  //       return;
+  //     }
+  //     log(chalk.hex("#646cff")(`Git push mirror successful.`));
+  //   });
+
+  const gitPushCommand = "git push origin master && git push mirror master"; // 同时推送所有数据源,刚刚测试这个指令可以执行
   exec(gitPushCommand, (error, stdout, stderr) => {
     if (error) {
       log(
@@ -38,17 +70,14 @@ exec(gitPullCommand, (error, stdout, stderr) => {
       );
       return;
     }
-    log(chalk.hex("#646cff")(`Git push origin successful.`));
-  });
+    // 在执行 Git push 操作时收到的错误消息 "Git push stderr: remote: Powered by GITEE.COM [GNK-6.4]" 表示你正在将更改推送到 Gitee（码云）仓库，并且这个错误消息来自于 Gitee 服务器的响应。
 
-  exec(gitPushMirrorCommand, (error, stdout, stderr) => {
-    if (error) {
-      log(
-        chalk.hex("#646cff")(`Error executing 'git push mirror':`) +
-          chalk.red(error.message)
-      );
-      return;
-    }
-    log(chalk.hex("#646cff")(`Git push mirror successful.`));
+    // 这个错误消息本身并不表示推送操作失败，它只是 Gitee 服务器的通知消息。通常情况下，如果没有其他错误消息，那么你的推送操作应该已经成功完成。
+    // if (stderr) {
+    //   log(chalk.hex("#646cff")(`Git push --all [stderr]:`) + chalk.red(stderr));
+    //   return;
+    // }
+    // 推送成功
+    log(chalk.hex("#646cff")("Git push --all:") + chalk.green(` successful.`));
   });
 });
